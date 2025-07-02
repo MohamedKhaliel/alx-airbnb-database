625aef9d2378f23535f31e9a15be29fb925281aa
MohamedKhaliel <mohamedahmed201131@gmail.com>
2025-07-03 00:47:45 +0300

    task 2

--- database-adv-script/aggregations_and_window_functions.sql
+++ database-adv-script/aggregations_and_window_functions.sql
@@ -10,3 +10,18 @@ LEFT JOIN
     booking b ON p.property_id = b.property_id
 GROUP BY 
     p.p_name;
+
+
+
+
+SELECT user_id , COUNT(*) FROM booking GROUP BY user_id;
+SELECT 
+    p.p_name,
+    COUNT(b.booking_id) AS booking_count,
+    ROW_NUMBER() OVER (PARTITION BY p.p_name ORDER BY COUNT(b.booking_id) ASC) AS rank_in_name
+FROM 
+    property p
+LEFT JOIN 
+    booking b ON p.property_id = b.property_id
+GROUP BY 
+    p.p_name;
